830913e6a777d6a6dc32f08aad2ea9367e997409
Alexandre Daoud <alex.daoud@mac.com>
2015-08-17 04:30:08 +0100

    fix file path

--- lib/bazaar.rb
+++ lib/bazaar.rb
@@ -20,7 +20,7 @@ module Bazaar
     (get_item("superadj") + ' ' + get_item("superitems")).humanize
   end
   def self.super_math_object
-    (get_item("superadj") + ' ' + get_item("supermathitems")).humanize
+    (get_item("superadj") + ' ' + get_item("mathitems")).humanize
   end
   def self.heroku
     get_item("superadj") + '-' + get_item("superitems") + '-' + rand(0-9999).to_s
